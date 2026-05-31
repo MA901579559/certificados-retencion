eedcb8df772665cfb73b9e8460952807ffb1a88a
MA901579559 <contador@masizo.co>
2026-05-30 20:11:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -59,10 +59,7 @@ if not CIUDAD_CONSIGNACION:
 fecha_emision = st.date_input("Fecha de emisión", value=date.today())
 fecha_texto = fecha_emision.strftime("%d de %b de %Y")
 
-archivo = st.file_uploader("Sube el auxiliar", type=["xlsx"])
 
-if archivo:
-    st.success("✅ Archivo cargado correctamente")
 
 # ---------------- FUNCIONES ----------------
 
@@ -112,6 +109,12 @@ def titulo(tipo):
     elif tipo == "ReteIVA":
         return "CERTIFICADO DE RETENCIONES DE IVA"
     return "CERTIFICADO"
+
+
+archivo = st.file_uploader("Sube el auxiliar", type=["xlsx"])
+
+if archivo:
+    st.success("✅ Archivo cargado correctamente")
 
 # ---------------- PROCESO ----------------
 
