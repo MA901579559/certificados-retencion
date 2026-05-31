51536a5c9bd81f4274d4ec1aa94d15058addc6c8
MA901579559 <contador@masizo.co>
2026-05-31 17:43:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ if archivo:
 
     df_head = pd.read_excel(archivo, engine="openpyxl", nrows=2, header=None)
     nombre_empresa_excel = str(df_head.iloc[1, 3]).strip()
-    st.write("### " + nombre_empresa_excel)
+    empresa_placeholder.markdown("### " + nombre_empresa_excel)
 
     df.columns = [
         "NitEmpresa","Cuenta","Nombre","Fecha",
