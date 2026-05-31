abdc6024699f5a9e750e2a2ce3cf201b597bd00f
MA901579559 <contador@masizo.co>
2026-05-30 21:48:32 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -91,10 +91,8 @@ def titulo(tipo):
 # ---------------- PROCESO ----------------
 
 if archivo:
-
-df_head = pd.read_excel(archivo, engine="openpyxl", nrows=2)
-nombre_empresa_excel = str(df_head.iloc[1, 3]).strip()
-
+    df_head = pd.read_excel(archivo, engine="openpyxl", nrows=2)
+    nombre_empresa_excel = str(df_head.iloc[1, 3]).strip()
     df = pd.read_excel(archivo, engine="openpyxl", skiprows=10)
 
     df.columns = [
