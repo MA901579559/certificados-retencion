c43185ecc6a781c4079e5e974f0260fd1e165fdb
MA901579559 <contador@masizo.co>
2026-05-31 16:17:59 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -24,9 +24,8 @@ st.title("📄 Generador Certificados de Retención")
 if nombre_empresa_excel:
     st.write("### " + nombre_empresa_excel)
 
-NOMBRE_EMPRESA = st.text_input("Nombre empresa", value="MASIZO SAS")
+# NOMBRE_EMPRESA = st.text_input("Nombre empresa", value="MASIZO SAS")
 ANIO = st.number_input("Año gravable", value=2026)
-
 CIUDAD_CONSIGNACION = st.text_input("Ciudad de consignación", value="Bogotá").strip()
 
 if not CIUDAD_CONSIGNACION:
