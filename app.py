b3d2f87a7508c282fe767db276fc30314079cea6
MA901579559 <contador@masizo.co>
2026-05-31 17:38:00 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -17,21 +17,14 @@ COL_TARIFA = 390
 COL_RET = ANCHO - MARGEN_DER - 5
 
 # ---------------- UI ----------------
-# ---------------- UI ----------------
 nombre_empresa_excel = ""
-NOMBRE_EMPRESA = ""
-
-archivo = st.file_uploader("Sube el auxiliar", type=["xlsx"])
-
-if archivo:
-    df_head = pd.read_excel(archivo, engine="openpyxl", nrows=2, header=None)
-    nombre_empresa_excel = str(df_head.iloc[1, 3]).strip()
-    NOMBRE_EMPRESA = nombre_empresa_excel
 
 st.title("📄 Generador Certificados de Retención")
 
 if nombre_empresa_excel:
     st.write("### " + nombre_empresa_excel)
+
+NOMBRE_EMPRESA = st.text_input("Nombre empresa", value="MASIZO SAS")
 
 col1, col2, col3 = st.columns(3)
 
